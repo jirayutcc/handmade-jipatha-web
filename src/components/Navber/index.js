e8846727792e63030eb928b2e4537e001e636581
Jirayut Srisawangwong <jirayut_ssww@hotmail.com>
2021-05-01 18:17:29 +0700

    add Navber

---
+++ src/components/Navber/index.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from "react";
+import { FaBars } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import { animateScroll as scroll } from "react-scroll";
+import {
+  Nav,
+  NavberContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavberElements";
+
+const Navber = ({ toggle }) => {
+  const [scrollNav, setscrollNav] = useState(false);
+  
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setscrollNav(true);
+    } else {
+      setscrollNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, []);
+
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav scrollNav={scrollNav}>
+          <NavberContainer>
+            <NavLogo to="/" onClick={toggleHome}>
+              Handmade Jipatha
+            </NavLogo>
+            <MobileIcon onClick={toggle}>
+              <FaBars />
+            </MobileIcon>
+            <NavMenu>
+              <NavItem>
+                <NavLinks
+                  to="about"
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                >
+                  เกี่ยวกับ
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks
+                  to="services"
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                >
+                  สินค้า
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks
+                  to="contact"
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                >
+                  ติดต่อ
+                </NavLinks>
+              </NavItem>
+              {/* <NavItem>
+                <NavLinks
+                  to="signup"
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                >
+                  ติดต่อ
+                </NavLinks>
+              </NavItem> */}
+            </NavMenu>
+            <NavBtn>
+              <NavBtnLink
+                to="/signin"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                เข้าสู่ระบบ
+              </NavBtnLink>
+            </NavBtn>
+          </NavberContainer>
+        </Nav>
+      </IconContext.Provider>
+    </>
+  );
+};
+
+export default Navber;
