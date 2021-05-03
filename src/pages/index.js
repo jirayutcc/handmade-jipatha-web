35f5ca2adee730f57e3c1b0c8a23d52f34702b0c
Jirayut Srisawangwong <jirayut_ssww@hotmail.com>
2021-05-03 16:37:10 +0700

    add index

---
+++ src/pages/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Sidebar from "../components/Sidebar";
+import Navber from "../components/Navber";
+import VideoSection from "../components/VideoSection";
+import InfoSection from "../components/InfoSection";
+import Services from "../components/Services";
+import Footer from "../components/Footer";
+
+import {
+  homeObjecOne,
+  homeObjecTwo,
+  // homeObjecThree,
+} from "../components/InfoSection/Data";
+
+const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navber toggle={toggle} />
+      <VideoSection />
+      <InfoSection {...homeObjecOne} />
+      <Services />
+      <InfoSection {...homeObjecTwo} />
+      {/* <InfoSection {...homeObjecThree} /> */}
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
